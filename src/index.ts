047c37a7c9c5ed6202bf020b0e26986924a3d022
SeanKim <sean.kim84@gmail.com>
2018-11-29 09:25:42 +0700

    Configuring the Server

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import dotenv from 'dotenv';
+    dotenv.config();
+    
+import { Options } from 'graphql-yoga';
+import { createConnection } from 'typeorm';
+import app from './app';
+import connectionOptions from './ormConfig';
+
+const PORT: number | string = process.env.PORT || 4000;
+const PLAYGROUND : string  = '/playground';
+const GRAPHQL_ENDPOINT: string = '/graphql';
+
+const appOptions: Options = {
+    port: PORT,
+    playground: PLAYGROUND,
+    endpoint: GRAPHQL_ENDPOINT
+};
+
+const handleAppStart = () => console.log(`Listening the PORT ${PORT}`);
+
+createConnection(connectionOptions).then(() => {
+    app.start(appOptions, handleAppStart)
+}).catch(error => console.log(error))
