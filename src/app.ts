d00ac34fef174c6f530f831eabc2e07f56803aae
SeanKim <sean.kim84@gmail.com>
2018-12-03 19:40:41 +0700

    Adding the jwtMiddleware

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import { GraphQLServer } from "graphql-yoga";
 import helmet from "helmet";
 import logger from "morgan";
 import schema from './schema';
+import decodeJWT from "./utils/decodeJWT";
 
 class App {
     public app: GraphQLServer;
@@ -16,7 +17,17 @@ class App {
         this.app.express.use(cors());
         this.app.express.use(logger("dev"));
         this.app.express.use(helmet());
+        this.app.express.use(this.jwt);
     };
+    
+    private jwt = async (req, res, next): Promise<void> => { // jwt를 위한 middleware 생성, next란 req가 아무것도 갖고 있지않으면 넘어가는것
+        const token = req.get("X-JWT"); // HEADER에서 받아올 이름
+        if(token){
+            const user = await decodeJWT(token);
+            console.log(user);
+        }
+        next();
+    }
 }
 
 export default new App().app;
