fbc0210f64ac2233214361e3e00d2e3fa8ae79fe
SeanKim <sean.kim84@gmail.com>
2018-12-04 12:40:50 +0700

    Resolver Middleware

--- src/api/User/GetMyProfile/GetMyProfile.resolvers.ts
+++ src/api/User/GetMyProfile/GetMyProfile.resolvers.ts
@@ -1,15 +1,16 @@
 import { Resolvers } from "../../../types/resolvers";
+import privateResolver from "../../../utils/privateResolver"; 
 
 const resolvers: Resolvers = {
   Query: {
-    GetMyProfile: async (_, __, { req }) => {
+    GetMyProfile: privateResolver(async (_, __, { req }) => {
       const { user } = req; // 아래처럼 user라는 객체를 하나만 사용하기 위해서 {} 사용.
       return {
         ok: true,
         error: null,
         user
       };
-    }
+    })
   }
 };
 
